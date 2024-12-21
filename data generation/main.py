f0b0182f53f3f7eea4e7fb5839957e305c0fe8d3
adekwal <adam.marek.walocha@gmail.com>
2024-12-21 12:46:45 +0100

    21.12
    Generate dataset with low RAM memory usage and less disc space required

--- data generation/main.py
+++ data generation/main.py
@@ -2,11 +2,11 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import imageio.v2 as imageio
+import h5py
 from training_data_gen import generate_data
 
-## Define simulation parameters
-data_path = r"C:\Users\jw\Desktop\projekty\praca_MC\GS_DNN\training_data\flowers"
-training_dirs = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
+# Define simulation parameters
+data_path = r"C:\Users\Monika Walocha\Desktop\adek files\_python\praca_inzynierska\flowers"
 nx = int(1024 / 2)
 ny = int(1024 / 2)
 px = 1024  # padded image size
@@ -15,85 +15,123 @@ dx = 2.4
 dy = dx
 n0 = 1
 lambda_ = 0.561
-delta_z = 8e3
-z1 = 3.5e3
+delta_z = 8.2222e3
+z1 = 3.5578e3
 z2 = z1 + delta_z
 delta_ph_max = np.pi / 2
-img_count = 20# 4242
 
-## Define user setup
-do_you_want_to_print_your_result = False # change it if you want to show images
+# Define user setup
+output_file = 'dane'  # define the filename
+img_count = 50  # choose the number of images you want to process
+save_as_npz = False
+save_as_h5 = True
+save_disc_space = False  # compress data file
+show_images = False  # show generated data
 
-## Checking propagation condition for angular spectrum
+# Checking propagation condition for angular spectrum
 print(lambda_ * delta_z < min([px, py]) * dx * dx)
 print(lambda_ * z2 < min([px, py]) * dx * dx)
+print()
 
-## Initial configurations
-inputs = []
-targets = []
-phase1 = []
-phase0 = []
+# Initialize containers for NPZ
+if save_as_npz:
+    inputs = []
+    targets = []
+    phase0 = []
+    phase1 = []
 
-## Generate GS data
-img_total_no = 0
-directories = [d for d in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, d))]
+# HDF5 setup
+if save_as_h5:
+    h5_file = f'{output_file}.h5'
 
-for i_dir in directories:
-    img_files = [f for f in os.listdir(os.path.join(data_path, i_dir)) if
-                 os.path.isfile(os.path.join(data_path, i_dir, f))]
+    with h5py.File(h5_file, 'w') as h5f:
+        h5f.create_dataset('inputs', shape=(0, nx, ny, 1), maxshape=(None, nx, ny, 1), compression=None,
+                           dtype='float32')
+        h5f.create_dataset('targets', shape=(0, nx, ny, 1), maxshape=(None, nx, ny, 1), compression=None,
+                           dtype='float32')
+        h5f.create_dataset('phase0', shape=(0, nx, ny, 1), maxshape=(None, nx, ny, 1), compression=None,
+                           dtype='float32')
+        h5f.create_dataset('phase1', shape=(0, nx, ny, 1), maxshape=(None, nx, ny, 1), compression=None,
+                           dtype='float32')
 
-    for i_img in range(0, int(np.ceil(img_count / len(directories)))):
-        img_total_no += 1
-        if img_total_no > img_count:
-            break
+# Process each image
+img_files = [f for f in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, f))]
+img_files = img_files[:img_count]
 
-        # Load image
-        img = imageio.imread(os.path.join(data_path, i_dir, img_files[i_img])).astype(float)
-        i1, i2, ph1, ph0 = generate_data(img, delta_ph_max, z1, z2, lambda_, dx, nx, ny, px, py)
+for img_no, img_file in enumerate(img_files, start=1):
+    img = imageio.imread(os.path.join(data_path, img_file)).astype(float)
+    i1, i2, ph1, ph0 = generate_data(img, delta_ph_max, z1, z2, lambda_, dx, nx, ny, px, py)  # generate data
 
-        # Append the processed image data to their respective lists, adding a new axis to match the desired shape
+    # Save data to NPZ lists
+    if save_as_npz:
         inputs.append(i1[..., np.newaxis])
         targets.append(i2[..., np.newaxis])
-        phase1.append(ph1[..., np.newaxis])
         phase0.append(ph0[..., np.newaxis])
+        phase1.append(ph1[..., np.newaxis])
 
-        if do_you_want_to_print_your_result:
-            i_range = [np.min(i1), np.max(i1)]
-            ph_range = [np.min(ph0), np.max(ph0)]
+    # Save data using HDF5
+    if save_as_h5:
+        with h5py.File(h5_file, 'a') as h5f:
+            for dataset_name, data in zip(['inputs', 'targets', 'phase0', 'phase1'], [i1, i2, ph0, ph1]):
+                dset = h5f[dataset_name]
+                dset.resize((dset.shape[0] + 1), axis=0)
+                dset[-1] = data[..., np.newaxis]
 
-            plt.figure(1, figsize=(10, 6))
-            plt.clf()
-            plt.suptitle(f'Image #{img_total_no}')
+    # Display images
+    if show_images:
+        i_range = [np.min(i1), np.max(i1)]
+        ph_range = [np.min(ph0), np.max(ph0)]
 
-            plt.subplot(2, 2, 1)
-            plt.imshow(i1, vmin=i_range[0], vmax=i_range[1], cmap='hot')
-            plt.title('i1')
-            plt.axis('image')
+        plt.figure(1, figsize=(10, 6))
+        plt.clf()
+        plt.suptitle(f'Image #{img_no}')
 
-            plt.subplot(2, 2, 2)
-            plt.imshow(i2, vmin=i_range[0], vmax=i_range[1], cmap='hot')
-            plt.title('i2')
-            plt.axis('image')
+        plt.subplot(2, 2, 1)
+        plt.imshow(i1, vmin=i_range[0], vmax=i_range[1], cmap='hot')
+        plt.title('i1')
+        plt.axis('image')
 
-            plt.subplot(2, 2, 3)
-            plt.imshow(ph0, vmin=ph_range[0], vmax=ph_range[1], cmap='spring')
-            plt.title('ph0')
-            plt.axis('image')
+        plt.subplot(2, 2, 2)
+        plt.imshow(i2, vmin=i_range[0], vmax=i_range[1], cmap='hot')
+        plt.title('i2')
+        plt.axis('image')
 
-            plt.subplot(2, 2, 4)
-            plt.imshow(ph1, vmin=ph_range[0], vmax=ph_range[1], cmap='spring')
-            plt.title('ph1')
-            plt.axis('image')
+        plt.subplot(2, 2, 3)
+        plt.imshow(ph0, vmin=ph_range[0], vmax=ph_range[1], cmap='spring')
+        plt.title('ph0')
+        plt.axis('image')
 
-            plt.pause(1)
+        plt.subplot(2, 2, 4)
+        plt.imshow(ph1, vmin=ph_range[0], vmax=ph_range[1], cmap='spring')
+        plt.title('ph1')
+        plt.axis('image')
 
-        print(img_total_no)
+        plt.pause(1)
 
-# Conversion of lists to NumPy arrays
-inputs = np.stack(inputs, axis=0)
-targets = np.stack(targets, axis=0)
-phase1 = np.stack(phase1, axis=0)
-phase0 = np.stack(phase0, axis=0)
+    print(f"{img_no}/{img_count}")
 
-np.savez('training_data.npz', inputs=inputs, targets=targets, phase0=phase0, phase1=phase1)
-print("\ndone")
+# Save data using NumPy
+if save_as_npz:
+    npz_file = f'{output_file}.npz'
+    if save_disc_space:
+        np.savez_compressed(npz_file, inputs=inputs, targets=targets, phase0=phase0, phase1=phase1)
+    else:
+        np.savez(npz_file, inputs=inputs, targets=targets, phase0=phase0, phase1=phase1)
+    print(f"\nData has been saved as: {npz_file}")
+
+# Close HDF5 file
+if save_as_h5:
+    h5f.close()
+    print(f"\nData has been saved as: {h5_file}")
+
+# Compress data file
+if save_as_h5 and save_disc_space:
+    with h5py.File(h5_file, 'r') as f_in:
+        with h5py.File(f'{output_file}_compressed.h5', 'w') as f_out:
+            for dataset_name in f_in:
+                data = f_in[dataset_name][:]
+
+                f_out.create_dataset(dataset_name, data=data, compression='gzip', compression_opts=9, dtype=data.dtype)
+
+    print(f"File '{output_file}.h5' has been compressed.")
+
