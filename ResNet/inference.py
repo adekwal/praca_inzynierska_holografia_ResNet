20a8e45f6a3ebe7b10b6051a3debe88b8d390e09
adekwal <adam.marek.walocha@gmail.com>
2024-12-29 12:01:00 +0100

    Operate with .h5 file

--- ResNet/inference.py
+++ ResNet/inference.py
@@ -1,22 +1,45 @@
-from resnet_regression import init_model
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import h5py
 
-# initialize model
-# model = init_model()
-# # load model
-# model.load_weights('trained_net.weights.h5')
-model = tf.keras.models.load_model('1000images_1epoch_model_checkpoint.keras')
+model = tf.keras.models.load_model(
+    r"C:\Users\Monika Walocha\Desktop\adek files\_python\ResNet\trained_models\epoch_10_model_checkpoint.keras"
+)
 
 try:
-    # Load the .npz file
-    data = np.load(
-        "C:/Users/jw/Desktop/dyplomy/Adam Walocha/AdamWalochaGitRep/praca_inzynierska_ResNet/training_data_100.npz")
+    with h5py.File(
+        r"C:/Users/Monika Walocha/Desktop/adek files/_python/praca_inzynierska/dane50_compressed.h5",
+        "r",
+    ) as file:
+        inputs = file["inputs"][:]
+        targets = file["targets"][:]
 
-    # Extract the inputs and targets
-    i1 = data['inputs'][10, :, :, 0]
-    i2 = data['targets'][10, :, :, 0]
+    i1 = inputs[8, :, :, 0]
+    i2 = targets[8, :, :, 0]
+
+    input_data = i1[np.newaxis, :, :, np.newaxis]
+
+    i2_predicted = model.predict(input_data, verbose=3) # Expected shape (None, 512, 512, 1)
+    i2_predicted = np.squeeze(i2_predicted)
+
+    vmin = min(i1.min(), i2.min(), i2_predicted.min())
+    vmax = max(i1.max(), i2.max(), i2_predicted.max())
+
+    fig1, axs = plt.subplots(1, 3, figsize=(15, 8))
+    im1 = axs[0].imshow(i1, vmin=vmin, vmax=vmax)
+    axs[0].set_title("Intensity at z1")
+
+    im2 = axs[1].imshow(i2, vmin=vmin, vmax=vmax)
+    axs[1].set_title("Intensity at z2")
+
+    im3 = axs[2].imshow(i2_predicted, vmin=vmin, vmax=vmax)
+    axs[2].set_title("Predicted intensity at z2")
+
+    cbar = fig1.colorbar(im3, ax=axs, location='right', shrink=0.8)
+    cbar.set_label("Intensity Value")
+
+    plt.show()
 
 except FileNotFoundError:
     raise FileNotFoundError("The dataset file was not found.")
@@ -25,20 +48,3 @@ except KeyError as e:
 except Exception as e:
     raise RuntimeError(f"An error occurred while loading the dataset: {e}")
 
-input_data = i1[np.newaxis, :, :, np.newaxis]
-i2_predicted = model.predict(input_data, verbose=3) # Expected shape (None, 512, 512, 1)
-i2_predicted = np.squeeze(i2_predicted)
-
-fig1, axs = plt.subplots(1, 3, figsize=(15, 8))
-im1 = axs[0].imshow(i1)
-fig1.colorbar(im1, ax=axs[0])
-axs[0].set_title("Intensity at z1")
-
-im2 = axs[1].imshow(i2)
-fig1.colorbar(im2, ax=axs[1])
-axs[1].set_title("Intensity at z2")
-
-im2 = axs[2].imshow(i2_predicted)
-fig1.colorbar(im2, ax=axs[2])
-axs[2].set_title("Predicted intensity at z2")
-plt.show()
